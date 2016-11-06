96d91651b716a373aef9603b2cf4b87c29b108d6
java-war <aweder@126.com>
2016-11-06 11:09:04 +0800

    添加config-server

---
+++ config-server/src/main/java/com/jwar/springcloud/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.jwar.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by XieJihong on 2016/11/6.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
