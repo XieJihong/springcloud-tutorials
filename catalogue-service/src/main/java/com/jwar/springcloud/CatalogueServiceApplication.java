8bdad0ee6bf9ad13e67d2f5f010ebb06284c03f4
java-war <aweder@126.com>
2016-11-07 23:06:44 +0800

    添加父pom文件

---
+++ catalogue-service/src/main/java/com/jwar/springcloud/CatalogueServiceApplication.java
@@ -0,0 +1,14 @@
+package com.jwar.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//@EnableHystrix
+public class CatalogueServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogueServiceApplication.class, args);
+    }
+}
